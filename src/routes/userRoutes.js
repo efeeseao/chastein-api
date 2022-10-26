4e8177f08d17d4cc118c130c0d527f0266d4e6fa
Fernando dos Santos <nando4803@gmail.com>
2022-10-26 23:15:54 +0100

    chore: add verifyJWT

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const usersController = require('../controllers/usersControllers')
+const verifyJWT = require('../middleware/verifyJWT')
+
+router.use(verifyJWT)
 
 router
   .route('/')

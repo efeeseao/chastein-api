6e70e9a0ee776ded465fe60d52ee44b6af3e7d04
Fernando dos Santos <nando4803@gmail.com>
2022-10-26 21:18:16 +0100

    feat: add server.js

---
+++ src/server.js
@@ -0,0 +1,59 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const { logger, logEvents } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const corsOptions = require('./config/cors.Options')
+const connectDB = require('./config/dbConnection')
+const root = require('./routes/root')
+const userRoutes = require('./routes/userRoutes')
+const noteRoutes = require('./routes/noteRoutes')
+
+const PORT = process.env.PORT || 3333
+
+connectDB()
+
+const app = express()
+
+app.use(logger)
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+
+app.use('/', express.static(path.join(__dirname, '../public')))
+
+app.use('/', root)
+app.use('/users', userRoutes)
+app.use('/notes', noteRoutes)
+
+app.use('*', (request, response) => {
+  response.status(404)
+  if (request.accepts('html')) {
+    response.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (request.accepts('join')) {
+    response.json({ message: '404 Not Found' })
+  } else response.type('text').send('404 Not Foud')
+})
+
+app.use(errorHandler)
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB!')
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+})
+
+mongoose.connection.on('error', (error) => {
+  console.log(error)
+  logEvents(
+    `${error.no}: ${error.code}\t${error.syscall}\t${error.hostname}`,
+    'mongoErrorLog.log'
+  )
+})

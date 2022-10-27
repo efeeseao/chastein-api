faf8217f549c4e4d755fc17e6601cb73cac30808
Fernando dos Santos <nando4803@gmail.com>
2022-10-27 16:37:45 +0100

    refactor: refactor

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,128 +1,113 @@
 const bcrypt = require('bcrypt')
 
-const Note = require('../models/Note')
 const User = require('../models/User')
+const Note = require('../models/Note')
+
 
 const getAllUsers = async (request, response) => {
+  // Get all users from MongoDB
   const users = await User.find().select('-password').lean()
 
   if (!users?.length) {
-    return response.status(400).json({ message: 'No user found!' })
+    return response.status(400).json({ message: 'No users found' })
   }
 
   response.json(users)
 }
 
 const createNewUser = async (request, response) => {
-  const { username, email, password, roles } = request.body
+  const { email, username, password, roles } = request.body
 
-  // Confirm data
-  if (
-    !username ||
-    !password ||
-    !email ||
-    !Array.isArray(roles) ||
-    !roles.length
-  ) {
-    return response.status(400).json({ message: 'All fields must be provided' })
+  if (!username || !email || !password) {
+    return response.status(400).json({ message: 'All fields are required' })
   }
 
-  //Check for duplicate email
-  const duplicateuUsername = await User.findOne({ username }).lean().exec()
+  // Check for duplicate username
+  const duplicateUsername = await User.findOne({ username }).collation({ locale: 'pt', strength: 2 }).lean().exec()
 
-  if (duplicateuUsername) {
+  if (duplicateUsername) {
     return response.status(409).json({ message: 'Duplicate username' })
   }
 
-  // Hash password
   const hashedPassword = await bcrypt.hash(password, 12)
 
-  const userObject = { email, username, password: hashedPassword, roles }
+  const userObject = (!Array.isArray(roles) || !roles.length)
+    ? { username, "password": hashedPassword, email }
+    : { username, "password": hashedPassword, roles, email }
 
-  // Create and store new User
   const user = await User.create(userObject)
 
   if (user) {
-    response
-      .status(201)
-      .json({ message: `User ${username} created successfully!` })
+    response.status(201).json({ message: `New user ${username} created` })
   } else {
-    response.status(400).json({ message: 'Invalid user data recived!' })
+    response.status(400).json({ message: 'Invalid user data received' })
   }
 }
 
-const upateUser = async (request, response) => {
-  const { id, username, email, roles, active, password } = request.body
-
-  // Confirm data
-  if (
-    !id ||
-    !username ||
-    !email ||
-    !Array.isArray(roles) ||
-    !roles.length ||
-    typeof active !== 'boolean'
-  ) {
-    return response.status(400).json({ message: 'All fields are required!' })
+const updateUser = async (request, response) => {
+  const { id, username, roles, active, password } = request.body
+ 
+  if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean') {
+    return response.status(400).json({ message: 'All fields except password are required' })
   }
 
   const user = await User.findById(id).exec()
 
   if (!user) {
-    return response.status(400).json({ message: 'User not found!' })
+      return response.status(400).json({ message: 'User not found' })
   }
 
-  //Check for duplicate email
-  const duplicateUsername = await User.findOne({ username }).lean().exec()
+  // Check for duplicate 
+  const duplicateUsername = await User.findOne({ username }).collation({ locale: 'pt', strength: 2 }).lean().exec()
 
-  // Allow update to the original user
+  // Allow updates to the original user 
   if (duplicateUsername && duplicateUsername?._id.toString() !== id) {
-    return response.status(409).json({ message: 'Duplicate username' })
+    return res.status(409).json({ message: 'Duplicate username' })
   }
 
   user.username = username
   user.roles = roles
-  user.email = email
   user.active = active
 
   if (password) {
     user.password = await bcrypt.hash(password, 12)
+
+    const updatedUser = await user.save()
+
+    response.json({ message: `${updatedUser.username} updated` })
   }
-
-  const updateUser = await user.save()
-
-  response.json({ message: `${updateUser.username} updated` })
 }
 
 const deleteUser = async (request, response) => {
   const { id } = request.body
 
   if (!id) {
-    return response.status(400).json({ message: 'User ID is required!' })
+    return response.status(400).json({ message: 'User ID Required' })
   }
 
+  // Does the user still have assigned notes?
   const note = await Note.findOne({ user: id }).lean().exec()
+    if (note) {
+      return response.status(400).json({ message: 'User has assigned notes' })
+    }
 
-  if (note?.length) {
-    return response.status(400).json({ message: 'User has assigned notes!' })
-  }
+    // Does the user exist to delete?
+    const user = await User.findById(id).exec()
 
-  const user = await User.findById(id).exec()
+    if (!user) {
+        return response.status(400).json({ message: 'User not found' })
+    }
 
-  if (!user) {
-    return response.status(400).json({ message: 'User does not exist!' })
-  }
+    const result = await user.deleteOne()
 
-  const result = await user.deleteOne()
+    const reply = `Username ${result.username} with ID ${result._id} deleted`
 
-  const reply = `Username ${result.username} with ID ${result._id} was deleted!`
-
-  response.json(reply)
+    response.json(reply)
 }
 
 module.exports = {
   getAllUsers,
   createNewUser,
-  upateUser,
+  updateUser,
   deleteUser
 }

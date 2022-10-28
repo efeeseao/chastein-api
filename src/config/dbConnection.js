3f49c7669aaa190c830d8281d2543bbe58f3f553
Fernando dos Santos <nando4803@gmail.com>
2022-10-28 16:10:40 +0100

    chore: dbconnection

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DATABASE_URI)
-  } catch (error) {
-    console.log(error)
-  }
+ mongoose.connect(process.env.DATABASE_URI).then(() => {
+  console.log('Database Connected')
+ }).catch(() => {
+  console.log('Database Not Connected')
+ })
 }
 
 module.exports = connectDB
